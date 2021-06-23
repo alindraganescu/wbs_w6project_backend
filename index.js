8a51ba97b7e251fdf87a0fa9dc0f08f5870caace
Alin Sergiu Draganescu <alindraganescu@gmail.com>
2021-06-23 18:12:16 +0200

    authors to be made

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+app.use(express.json()); //to be able to work with data
 const fs = require('fs');
-const cors = require('cors');
-
-app.use(express.json());
+const cors = require('cors'); //to avoid security problems between browser and back-end
 app.use(cors());
 
 app.get('/', (req, res) => {
   fs.readFile('./data.json', 'utf-8', (err, data) => {
+    //if the root is requested the data is sent
     if (err) {
       res.status(500).send('Failed to load the data.');
     } else {
@@ -19,6 +18,26 @@ app.get('/', (req, res) => {
 
 app.get('/wiki', (req, res) => {
   fs.readFile('./data_wiki.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).send('Failed to load the data.');
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get('/author', (req, res) => {
+  fs.readFile('./data_authors.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).send('Failed to load the data.');
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.get('/author/:id', (req, res) => {
+  fs.readFile('./data_authors.json', 'utf-8', (err, data) => {
     if (err) {
       res.status(500).send('Failed to load the data.');
     } else {
